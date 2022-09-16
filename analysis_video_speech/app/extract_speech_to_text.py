f11a94d51257a9d1806501a1ad537bee4ec8ce8a
fxa76 <francois_albouy@epam.com>
2022-09-16 18:08:49 +0200

    init analysis video speech

---
+++ analysis_video_speech/app/extract_speech_to_text.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+# usage : python x.py
+# depends on : postgresql server being available
+
+# this program is made to be run incrementally (ie many times without destroying previous run)
+# 1. detect and insert objects for images with no objects already detected
+
+# check this code to make a better threaded  : https://github.com/pika/pika/blob/0.12.0/examples/basic_consumer_threaded.py
+import sys
+
+import os
+import platform
+import json
+import pika
+import logging.config
+import time
+
+import threading
+import multiprocessing
+
+#local imports
+import video_analyzer as video_analyzer
+
+from flask import Flask
+
+app = Flask(__name__,)
+
+@app.route('/')
+def index():
+    return "Video analyzer console"
+
+@app.route('/health')
+def health():
+    return ''
+
+analyzer = 'video_speech_to_text'
+analyzer_version = 100
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger(__name__)
+
+def post_task(obj):
+    credentials = pika.PlainCredentials('mime_engine', '908790172834598012374')
+    parameters = pika.ConnectionParameters('rabbitmq',
+                                   5672,
+                                   '/',
+                                   credentials)
+    connection_out = pika.BlockingConnection(parameters)
+    channel_out = connection_out.channel()
+    channel_out.queue_declare(queue='addObject', durable=True)
+    channel_out.basic_publish(exchange='',
+                          routing_key='addObject',
+                          body=json.dumps(obj),
+                          properties=pika.BasicProperties(
+                            content_type='application/json',
+                            content_encoding='utf-8',
+                            delivery_mode = 2 # make message persistent
+                        ))
+    connection_out.close()
+
+def detect_text(ch, method, properties, body):
+    obj = json.loads(body)
+    filefullname = obj['filefullname']
+    image_id = obj['image_id']
+
+    logger.debug("analyzing video : {} {}".format(image_id, filefullname))
+    try:
+        try:
+            text = video_analyzer.get_text(filefullname)
+            logger.debug("text exctracted from speech : {}".format(text))
+            if len(text.replace(' ', '')) > 0:
+                logger.debug("Pushing OBJ {}".format(obj))
+                obj_task = {
+                    "filefullname":filefullname,
+                    "image_id":image_id,
+                    "x":0,
+                    "y":0,
+                    "w":0,
+                    "h":0,
+                    "description":"speech_to_text",
+                    "confidence":80,
+                    "analyzer":analyzer,
+                    "analyzer_version":analyzer_version,
+                    "derived_from_object":None,
+                    "object_image_filename":None,
+                    "face_encodings":None,
+                    "text_found": text
+                }
+                post_task(obj_task)
+                logger.debug("Pushed OBJ {}".format(obj_task))
+        except (Exception) as error:
+            obj_task = { "filefullname":filefullname,"description":"error_on_detection","analyzer":analyzer,"analyzer_version":analyzer_version,"image_id":image_id}
+            post_task(obj_task)
+
+    except (Exception) as error:
+        logger.debug("error while processing image", exc_info=True)
+
+
+
+def receive(ch, method, properties, body):
+        logger.debug("""rabbitmq receiver""")
+        t = threading.Thread(target=detect_text, args=(ch, method, properties, body,))
+        t.daemon = True
+        t.start()
+
+        while t.is_alive():
+            logger.debug("[INFO] heart beating")
+            ch.connection.process_data_events()
+            ch.connection.sleep(5)
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        logger.debug("done")
+
+def start():
+    credentials = pika.PlainCredentials('mime_engine', '908790172834598012374')
+    parameters = pika.ConnectionParameters('rabbitmq',
+                                   5672,
+                                   '/',
+                                   credentials)
+
+    while(True):
+        try :
+            connection = pika.BlockingConnection(parameters)
+            channel = connection.channel()
+            channel.queue_declare(queue='extractSpeech', durable=True)
+            channel.basic_qos(prefetch_count=1)
+            channel.basic_consume(queue='extractSpeech', on_message_callback=receive, auto_ack=False)
+
+            logger.info(' [*] Waiting for messages on extractText. To exit press CTRL+C')
+            channel.start_consuming()
+        except (Exception) as error:
+            logger.error('Rabbitmq connection failed', exc_info=True)
+            time.sleep(5)
+
+if __name__ == '__main__':
+    logger.info("starting to extract speech")
+
+
+    def server_start():
+        app.run(debug=True, host="0.0.0.0", port=6000)
+
+
+    p = multiprocessing.Process(target=server_start, args=())
+    p.start()
+
+
+    start()
+    logger.info("done")
---
+++ analysis_video_speech/app/video_analyzer.py
@@ -0,0 +1,110 @@
+# Python code to convert video to audio
+# code original source : https://www.thepythoncode.com/article/using-speech-recognition-to-convert-speech-to-text-python
+import moviepy.editor as mp
+import speech_recognition as sr
+
+import os
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+from threading import Timer
+
+# a function that splits the audio file into chunks
+# and applies speech recognition
+def get_large_audio_transcription(path):
+    """
+    Splitting the large audio file into chunks
+    and apply speech recognition on each of these chunks
+    """
+    # open the audio file using pydub
+    sound = AudioSegment.from_wav(path)
+    # split audio sound where silence is 700 miliseconds or more and get chunks
+    chunks = split_on_silence(sound,
+                              # experiment with this value for your target audio file
+                              min_silence_len=500,
+                              # adjust this per requirement
+                              silence_thresh=sound.dBFS - 14,
+                              # keep the silence for 1 second, adjustable as well
+                              keep_silence=500,
+                              )
+    folder_name = "audio-chunks"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    # process each chunk
+    r = sr.Recognizer()
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                text = r.recognize_google(audio_listened, language="fr-FR")
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                print(chunk_filename, ":", text)
+                whole_text += text
+
+        #delete the chunk
+        os.remove(chunk_filename)
+    # return the text for all chunks detected
+    return whole_text
+
+def shortwav(audio_path):
+    # initialize the recognizermoviepy ffmp
+    r = sr.Recognizer()
+
+    # open the file
+    with sr.AudioFile(audio_path) as source:
+        # listen for the data (load audio to memory)
+        audio_data = r.record(source)
+        # recognize (convert from speech to text)
+        text = r.recognize_google(audio_data, language="fr-FR")
+        return text
+
+
+def extract_audio(video_file):
+    # Insert Local Video File Path
+    clip = mp.VideoFileClip(video_file)
+    # Insert Local Audio File Path
+    temp_file_path = "/tmp/data/test.wav"
+
+    # https://github.com/Zulko/moviepy/issues/803
+
+    def timeout_trigger():
+        print("killing process")
+        try:
+
+            clip.close()
+        except Exception as e:
+            print("too late")
+
+    #must monitor progress instead of killing after 30 seconds...
+    timer = Timer(30, timeout_trigger)
+    timer.start()
+    try:
+        clip.audio.write_audiofile(temp_file_path, codec='pcm_s16le', write_logfile=True)
+    except Exception as e:
+        print("Wav bug")
+
+    timer.cancel()
+    print(temp_file_path)
+    return temp_file_path
+
+
+def get_text(video_path):
+    audio_path = extract_audio(video_path)
+    #shortwav(audio_path)
+    return get_large_audio_transcription(audio_path)
+
+if __name__ == '__main__':
+    audio_path = extract_audio("/tmp/data/test.mov")
+    get_large_audio_transcription(audio_path)
+    shortwav(audio_path)
