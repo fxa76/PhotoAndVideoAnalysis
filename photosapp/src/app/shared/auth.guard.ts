495e353e1948de007bd57407cec14e33525067f3
fxa76 <francois_albouy@epam.com>
2024-09-25 13:01:03 +0200

    update to v16 @todo: change https://stackoverflow.com/questions/75564717/angulars-canactivate-interface-is-deprecated-how-to-replace-it

--- photosapp/src/app/shared/auth.guard.ts
+++ photosapp/src/app/shared/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { AuthService } from './../shared/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(
     public authService: AuthService,
     public router: Router
